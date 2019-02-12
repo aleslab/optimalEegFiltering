f398a734471c9f2975c3b09dfad0c527a3b8cfbd
Katherine Habel <katherinehabel@Katherines-MacBook-Pro-4.local>
2019-02-12 17:32:55 +0000

    added the results of filtering the condition without patterned noise

--- Data/average_v_filter.R
+++ Data/average_v_filter.R
@@ -4,5 +4,9 @@ MSE.v1.average <- mse(scenario.v1.average, signal(i = 64, j = 600, k = 1))
 scenario.v2.average <- apply(X = scenario.v2, MARGIN = c(1,2), FUN = mean)
 MSE.v2.average <- mse(scenario.v2.average, signal(i = 64, j = 600, k = 1))
 
+RNoise <- array(rnorm(64 * 600, mean = 0, sd = 1), dim = c(64, 600, 1))
+scenario.v1.filter <- GSVD_filter(scenario.v1, RNoise)
+MSE.v1.filter <- mse(scenario.v1.filter, signal(i = 64, j = 600, k = 1))
+
 scenario.v2.filter <- GSVD_filter(scenario.v2, Noise.v2)
 MSE.v2.filter <- mse(scenario.v2.filter, signal(i = 64, j = 600, k = 1))
