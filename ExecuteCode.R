3dd32039c3137076f721fe7f3a6103f79f873ef6
Katherine Habel <katherinehabel@katherinesmbp4.home>
2019-03-03 18:20:47 +0000

    made function that sources all functions in order to make it easier to run

---
+++ ExecuteCode.R
@@ -0,0 +1,15 @@
+source('~/Documents/Honours Project/SH Project/Data/signal_v1.R')
+
+source('~/Documents/Honours Project/SH Project/Data/simulation_v1.R')
+
+source('~/Documents/Honours Project/SH Project/Data/Noise_v2.R')
+
+source('~/Documents/Honours Project/SH Project/Data/simulation_v2.R')
+
+source('~/Documents/Honours Project/SH Project/R Functions/GSVD_filter.R')
+
+source('~/Documents/Honours Project/SH Project/R Functions/MSE.R')
+
+source('~/Documents/Honours Project/SH Project/Data/average_v_filter.R')
+
+
--- R Functions/MSE.R
+++ R Functions/MSE.R
@@ -1,5 +1,15 @@
 mse <- function(y_est, y_true) {
+# The purpose of this function is to calculate the mean squared error of two objects
+# The inputs are two objects for the MSE to be calculated for, one being the "true" 
+# value and the other being an estimate. The output is a value that quantifies the 
+# mean squared error
+  
+# Calculate the mean of the true
   mu <- mean(y_true)
+
+# average the squared difference of the estimate and the true mean
   error <- mean((y_est - y_true)^2)
+  
+# return MSE value
   return(error)
 }
